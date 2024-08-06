736deace468b57fac9ba77d8275271a3337bbb44
Babur <bgur@upei.ca>
2024-08-06 12:43:34 +0300

    Latest /app is from the modals tutorial

--- app-example-Nav-Tabs/(tabs)/index.tsx
+++ app-example-Nav-Tabs/(tabs)/index.tsx
@@ -1,9 +1,9 @@
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function DetailsScreen() {
+export default function Tab() {
   return (
     <View style={styles.container}>
-      <Text>Details</Text>
+      <Text>Tab [Home|Settings]</Text>
     </View>
   );
 }
